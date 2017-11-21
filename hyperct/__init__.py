ff5465fe55482403e991faff62f793bf5b801380
stefan-endres <stefan.c.endres@gmail.com>
2017-11-21 16:37:57 +0200

    Split rec and cube files, add n_cube init tests

---
+++ hyperct/__init__.py
@@ -0,0 +1,3 @@
+from hyperct._triangulation import *
+
+__all__ = [s for s in dir() if not s.startswith('_')]
---
+++ hyperct/_field.py
@@ -0,0 +1,3 @@
+"""Add homology properties of a field associated with the geometry built from
+the classes in _triangulation.py """
+
